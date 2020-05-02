79c7450f6deb97a5fb8d1b3a8a9af593d33dc119
William A Rodriguez <wrod@mit.edu>
2020-05-02 19:08:44 -0400

    skeleton ready

--- src/App.js
+++ src/App.js
@@ -4,21 +4,28 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container"
+    style = {{  backgroundColor: '#282c34',
+      minHeight: '100vh',
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+      justifyContent: 'center',
+      fontSize: 'calc(10px + 2vmin)',
+      color: 'white'}}
+    >
+      <h1>Movies</h1>
+      <div style={{display: 'flex',flexDirection:'row',width: "100%",alignItems: 'center',justifyContent: 'center'}}>
+        <input type="text" value={'...Extraction'} style={{width: '80%',height: '10vh',fontSize: '3vh'}}/>
+      </div>
+      <div style={{display: 'flex',flexDirection: 'row',width: 1000,height: 500,alignItems: 'center',justifyContent: 'center'}}>
+        <div style={{backgroundColor: 'blue',width: 200,height: 200}}></div>
+      </div>
+
+      <div style={{display: 'flex',flexDirection:'row',width: "100%",alignItems: 'center',justifyContent: 'center'}}>
+        <div>previous | current page number | next</div>
+      </div>
+
     </div>
   );
 }
